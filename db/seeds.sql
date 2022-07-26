fdcbaf6701ed12ac2a23bbc4b9e7ef3c203f8dac
Katie <katiebutler37@gmail.com>
2022-07-26 16:06:56 -0400

    starting up program

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+NSERT INTO departments (department_name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, '1'),
+  ('Software Engineer', 120000, '2'); 
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('John', 'Doe', 1, NULL),
+  ('Kevin', 'Tupik', 2, 1);
+ 
