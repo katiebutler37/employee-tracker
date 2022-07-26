d9fca5be33006468ac04d2dadb3d7eeb3ddce41a
Katie <katiebutler37@gmail.com>
2022-07-26 17:05:22 -0400

    debugging tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 -- this order is significant as well
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
 -- must declare tables that are referenced (their primary et for another table's foreign key) first, be mindful of table order
 CREATE TABLE departments (
@@ -13,6 +13,7 @@ CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
+  department_id INTEGER,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
@@ -20,7 +21,9 @@ CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+  role_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  manager_id INTEGER
+--   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
   -- need to adjust this one still for sure
 );
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
-const consoleTable = require('console.table');
+const table = require('console.table');
 
+//file for db queries in async promises from inquirer
+//make separate file for constructor functions 
+
+// inquirer
+//     .prompt ({
+//         //prompts here
+//     })
+//     .then ((answers) => {
+//         const employee = new Employee (answers.promptName1, answers.promptName2)
+//         console.table(s)
+//     })
